75aed3042d4a4afe872dd89abc889a6ba3e195c9
Vitor <frete497@gmail.com>
2023-03-28 19:37:13 -0400

    feat: create controllers folder, routes

---
+++ src/http/controllers/register.controller.ts
@@ -0,0 +1,23 @@
+import { FastifyRequest, FastifyReply } from "fastify"
+import { prisma } from "@/lib/prisma"
+import { z } from "zod"
+
+export async function registerController(request: FastifyRequest, reply: FastifyReply)  {
+  const registerBodyScheme = z.object ({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodyScheme.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    }
+  })
+
+  return reply.status(201).send()
+}
