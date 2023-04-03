9b43c9af4478f61f3dd0ec68f5a7d7f45ce41ffc
Vitor <frete497@gmail.com>
2023-04-03 19:58:33 -0400

    feat: implement create method in PrismaCheckInsRepository

--- src/repositories/prisma/prisma-check-ins-repository.ts
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -1,10 +1,13 @@
 import { Prisma, CheckIn } from "@prisma/client";
+import {prisma} from "@/lib/prisma";
 import { CheckinsRepository } from "../check-in-repository";
 
 export class PrismaCheckInsRepository implements CheckinsRepository{
   
   async create(data: Prisma.CheckInUncheckedCreateInput) {
-    throw new Error("Method not implemented.");
+    const checkIn = await prisma.checkIn.create({data})
+
+    return checkIn
   }
   async findByUserIdOnDate(userId: string, date: Date) {
     throw new Error("Method not implemented.");
@@ -16,7 +19,13 @@ export class PrismaCheckInsRepository implements CheckinsRepository{
     throw new Error("Method not implemented.");
   }
   async findById(id: string) {
-    throw new Error("Method not implemented.");
+    const checkIn = await prisma.checkIn.findUnique({
+      where: {
+        id
+      }
+    })
+
+    return checkIn
   }
   async save(checkIn: CheckIn) {
     throw new Error("Method not implemented.");
