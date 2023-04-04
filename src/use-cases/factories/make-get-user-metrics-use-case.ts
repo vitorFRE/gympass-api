96564370ca6f701ad6eb1644d82fa12cd7d30a12
Vitor <frete497@gmail.com>
2023-04-04 16:07:16 -0400

    feat: created makeCheckInUseCase factory

--- src/use-cases/factories/make-get-user-metrics-use-case.ts
+++ src/use-cases/factories/make-get-user-metrics-use-case.ts
@@ -1,7 +1,7 @@
 import { PrismaCheckInsRepository } from "@/repositories/prisma/prisma-check-ins-repository"
 import { GetUserMetricsUseCase } from "../get-user-metrics"
 
-export function makeGetUserProfileUseCase () {
+export function makeGetUserMetricsUseCase () {
   const checkInsRepository = new PrismaCheckInsRepository()
   const useCase = new GetUserMetricsUseCase(checkInsRepository)
 
