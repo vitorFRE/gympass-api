e6685adffad2476e5c93c2b7222b400ffa5ec80b
Vitor <frete497@gmail.com>
2023-04-04 15:35:00 -0400

    feat: implement findById method in PrismaGymsRepository

--- src/repositories/prisma/prisma-gyms-repository.ts
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -1,10 +1,15 @@
-import { Gym, Prisma } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import { FindManyNearByParams, GymsRepository } from "../gyms-repository";
+import { prisma } from "@/lib/prisma";
 
 export class PrismaGymsRepository implements GymsRepository {
   
   async findById(id: string) {
-    throw new Error("Method not implemented.");
+    const gym = await prisma.gym.findUnique({
+      where: {id}
+    })
+
+    return gym
   }
   async create(data: Prisma.GymCreateInput) {
     throw new Error("Method not implemented.");
