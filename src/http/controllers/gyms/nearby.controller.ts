18a9b30bfd052f9c5c23249f1f75b4811b23578c
Vitor <frete497@gmail.com>
2023-04-05 14:36:49 -0400

    feat: implement nearby gym controller and case factory integration

---
+++ src/http/controllers/gyms/nearby.controller.ts
@@ -0,0 +1,28 @@
+import { makeFetchNearbyGymsUseCase } from "@/use-cases/factories/make-fetch-nearby-gyms-use-case"
+import { FastifyRequest, FastifyReply } from "fastify"
+import { z } from "zod"
+
+export async function nearbyController(request: FastifyRequest, reply: FastifyReply)  {
+  const nearbyGymsScheme = z.object ({
+    latitude: z.number().refine(value => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine(value => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude} = nearbyGymsScheme.parse(request.body)
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+
+const {gyms} = await fetchNearbyGymsUseCase.execute({
+ userLatitude: latitude,
+ userLongitude: longitude
+})
+
+
+  return reply.status(200).send({
+    gyms
+  })
+}
